10e8cc9722321bf71529a43f8664922e095c44c4
shabnazpathan <pathan.2314513883@mujonline.edu.in>
2025-07-22 18:00:49 +0530

    Update PatientRepository.java

--- back-end/src/main/java/com/project_black_end/repo/PatientRepository.java
+++ back-end/src/main/java/com/project_black_end/repo/PatientRepository.java
@@ -1,1 +1,10 @@
-//patientRepository
+package com.project_black_end.repo;
+
+import com.project_black_end.models.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByEmail(String email);
+    Optional<Patient> findByEmailOrPhone(String email, String phone);
+}
