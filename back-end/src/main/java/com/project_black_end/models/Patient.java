4b850f34e8f40f3cdeac25e0c8c40e5fc0322b61
shabnazpathan <pathan.2314513883@mujonline.edu.in>
2025-07-22 17:49:11 +0530

    Create Patient.java

---
+++ back-end/src/main/java/com/project_black_end/models/Patient.java
@@ -0,0 +1,12 @@
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+    private String gender;
+    private String contact;
+
+    // Constructors, Getters, Setters
+}
