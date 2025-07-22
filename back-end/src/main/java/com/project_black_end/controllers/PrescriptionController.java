a1750ee13d929a48f9499d6e3de7e58f944074f4
shabnazpathan <pathan.2314513883@mujonline.edu.in>
2025-07-22 18:27:40 +0530

    Update PrescriptionController.java

--- back-end/src/main/java/com/project_black_end/controllers/PrescriptionController.java
+++ back-end/src/main/java/com/project_black_end/controllers/PrescriptionController.java
@@ -1,1 +1,21 @@
-//Prescription
+package com.project_black_end.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/prescriptions")
+public class PrescriptionController {
+
+    @PostMapping
+    public ResponseEntity<?> savePrescription(
+            @RequestHeader("Authorization") String token,
+            @RequestBody String prescriptionJson) {
+
+        if (!token.equals("VALID_TOKEN")) {
+            return ResponseEntity.status(403).body("Unauthorized");
+        }
+
+        return ResponseEntity.ok("Prescription saved");
+    }
+}
