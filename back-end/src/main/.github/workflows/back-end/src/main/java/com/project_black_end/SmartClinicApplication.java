1b5d5ea15f3185e3f42f19f663a38d06ead14046
shabnazpathan <pathan.2314513883@mujonline.edu.in>
2025-07-22 20:00:45 +0530

    Create SmartClinicApplication.java

---
+++ back-end/src/main/.github/workflows/back-end/src/main/java/com/project_black_end/SmartClinicApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+  public class SmartClincApplication{
+    public static void main(String[] args){
+      SpringApplication.run(SmartClinicApplication.class,args);
+    }
+  }
