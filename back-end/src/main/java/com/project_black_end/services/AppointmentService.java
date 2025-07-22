d7c945b456bca2dee9e7ecaec5e7d42214634acf
shabnazpathan <pathan.2314513883@mujonline.edu.in>
2025-07-22 18:24:38 +0530

    Update AppointmentService.java

--- back-end/src/main/java/com/project_black_end/services/AppointmentService.java
+++ back-end/src/main/java/com/project_black_end/services/AppointmentService.java
@@ -1,1 +1,29 @@
-//appointmentservice
+package com.project_black_end.services;
+
+import com.project_black_end.models.Appointment;
+import com.project_black_end.models.Doctor;
+import com.project_black_end.repo.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class AppointmentService {
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    public Appointment bookAppointment(Appointment appointment) {
+        return appointmentRepository.save(appointment);
+    }
+
+    public List<Appointment> getAppointmentsForDoctorOnDate(Doctor doctor, LocalDate date) {
+        return appointmentRepository.findByDoctorAndAppointmentTimeBetween(
+            doctor,
+            date.atStartOfDay(),
+            date.plusDays(1).atStartOfDay()
+        );
+    }
+}
