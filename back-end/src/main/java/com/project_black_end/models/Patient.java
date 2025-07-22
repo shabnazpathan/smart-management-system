ca2bcc114b224fb0c0f3dcdba4f02fbac688f18b
shabnazpathan <pathan.2314513883@mujonline.edu.in>
2025-07-22 18:29:07 +0530

    Update Patient.java

--- back-end/src/main/java/com/project_black_end/models/Patient.java
+++ back-end/src/main/java/com/project_black_end/models/Patient.java
@@ -1,12 +1,27 @@
+package com.project_black_end.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
 @Entity
+@Data
 public class Patient {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private int age;
-    private String gender;
-    private String contact;
 
-    // Constructors, Getters, Setters
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String phoneNumber;
+
+    @NotBlank
+    private String password;
 }
