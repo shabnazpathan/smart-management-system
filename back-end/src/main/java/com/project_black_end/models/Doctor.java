6a603d057d4f9f597518c3aa60c105d1834f736d
shabnazpathan <pathan.2314513883@mujonline.edu.in>
2025-07-22 18:29:25 +0530

    Update Doctor.java

--- back-end/src/main/java/com/project_black_end/models/Doctor.java
+++ back-end/src/main/java/com/project_black_end/models/Doctor.java
@@ -1,11 +1,24 @@
+package com.project_black_end.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import java.util.List;
+
 @Entity
+@Data
 public class Doctor {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String speciality;
-    private String contact;
 
-    // Constructors, Getters, Setters
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String specialization;
+
+    @ElementCollection
+    private List<String> availableTimes; // Stored as simple list
 }
