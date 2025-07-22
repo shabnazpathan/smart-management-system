ee683bcf25527e78fa6d1e857cc2a27ee016009f
shabnazpathan <pathan.2314513883@mujonline.edu.in>
2025-07-22 17:49:35 +0530

    Update Doctor.java

--- back-end/src/main/java/com/project_black_end/models/Doctor.java
+++ back-end/src/main/java/com/project_black_end/models/Doctor.java
@@ -1,4 +1,11 @@
-import java.io.util;
-public class Doctor(){
-  
+@Entity
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String speciality;
+    private String contact;
+
+    // Constructors, Getters, Setters
 }
