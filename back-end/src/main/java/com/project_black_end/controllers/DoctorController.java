6c003cf0147ef534faa16f7dc0c6d34a1e22f692
shabnazpathan <pathan.2314513883@mujonline.edu.in>
2025-07-22 18:28:01 +0530

    Update DoctorController.java

--- back-end/src/main/java/com/project_black_end/controllers/DoctorController.java
+++ back-end/src/main/java/com/project_black_end/controllers/DoctorController.java
@@ -1,1 +1,35 @@
-//Doctor Controller 
+package com.project_black_end.controllers;
+
+import com.project_black_end.models.Doctor;
+import com.project_black_end.services.DoctorService;
+import com.project_black_end.services.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/doctor")
+public class DoctorController {
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @GetMapping("/availability")
+    public ResponseEntity<?> getAvailableDoctors(
+            @RequestParam String specialization,
+            @RequestParam String time,
+            @RequestHeader("Authorization") String token) {
+
+        if (!tokenService.validateToken(token)) {
+            return ResponseEntity.status(401).body("Invalid token");
+        }
+
+        List<Doctor> doctors = doctorService.findAvailableDoctors(specialization, time);
+        return ResponseEntity.ok(doctors);
+    }
+}
