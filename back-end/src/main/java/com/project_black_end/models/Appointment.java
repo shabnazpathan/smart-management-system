bf9ed20cee67c98f8b6118699bb834d71fe33d5e
shabnazpathan <pathan.2314513883@mujonline.edu.in>
2025-07-22 17:49:55 +0530

    Update Appointment.java

--- back-end/src/main/java/com/project_black_end/models/Appointment.java
+++ back-end/src/main/java/com/project_black_end/models/Appointment.java
@@ -1,1 +1,18 @@
 //appointment
+@Entity
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    private Patient patient;
+
+    @ManyToOne
+    private Doctor doctor;
+
+    private LocalDateTime appointmentDate;
+    private String status;
+
+    // Constructors, Getters, Setters
+}
